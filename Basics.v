da7b1eaa0b27ecb7214f38d4ffe05db419864d71
Daniel Dyba <daniel.dyba@gmail.com>
2014-08-11 21:09:16 -0700

    Function types and numbers
    * Added new exercises

--- Basics.v
+++ Basics.v
@@ -96,3 +96,145 @@ Example test_andb33: (andb3 true false true) = false.
 Proof. reflexivity. Qed.
 Example test_andb34: (andb3 true true false) = false.
 Proof. reflexivity. Qed.
+
+(* Function Types *)
+
+Check true.
+Check (negb true).
+Check negb.
+
+(* Numbers *)
+
+Module Playground1.
+
+Inductive nat : Type :=
+| O : nat
+| S : nat -> nat.
+
+Definition pred (n:nat) : nat :=
+  match n with
+      | O => O
+      | S n' => n'
+  end.
+
+End Playground1.
+
+Definition minustwo (n:nat) : nat :=
+  match n with
+    | O => O
+    | S O => O
+    | S (S n') => n'
+  end.
+
+Check (S (S (S (S O)))).
+Eval compute in (minustwo 4).
+
+Check S.
+Check pred.
+Check minustwo.
+
+Fixpoint evenb (n:nat) : bool :=
+  match n with
+      | O => true
+      | S O => false
+      | S (S n') => evenb n'
+  end.
+
+Definition oddb (n:nat) : bool := negb (evenb n).
+
+Example test_oddb1: (oddb (S O)) = true.
+Proof. reflexivity. Qed.
+Example test_oddb2: (oddb (S (S (S (S O))))) = false.
+Proof. reflexivity. Qed.
+
+Module Playground2.
+
+Fixpoint plus (n:nat) (m:nat) : nat :=
+  match n with
+    | O => m
+    | S n' => S (plus n' m)
+  end.
+
+Eval compute in (plus (S (S (S O))) (S (S O))).
+
+Fixpoint mult (n m : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => plus m (mult n' m)
+  end.
+
+Example test_mult1: (mult 3 3) = 9.
+Proof. reflexivity. Qed.
+
+Fixpoint minus (n m : nat) : nat :=
+  match n, m with
+    | O, _ => O
+    | S _, O => n
+    | S n', S m' => minus n' m'
+  end.
+
+End Playground2.
+
+Fixpoint exp (base power: nat) : nat :=
+  match power with
+    | O => S O
+    | S p => mult base (exp base p)
+  end.
+
+(* Exercise: 1 star (factorial) *)
+
+Fixpoint factorial (n:nat) : nat :=
+  match n with
+    | O => S O
+    | S p => mult n (factorial p)
+  end.
+
+Example test_factorial1: (factorial 3) = 6.
+Proof. reflexivity. Qed.
+Example test_factorial2: (factorial 5) = (mult 10 12).
+Proof. reflexivity. Qed.
+
+Notation "x + y" := (plus x y)
+                      (at level 50, left associativity)
+                    : nat_scope.
+Notation "x - y" := (minus x y)
+                      (at level 50, left associativity)
+                    : nat_scope.
+Notation "x * y" := (mult x y)
+                      (at level 40, left associativity)
+                    : nat_scope.
+
+Check ((0 + 1) + 1).
+
+Fixpoint beq_nat (n m : nat) : bool :=
+  match n, m with
+    | O, O => true
+    | O, S m' => false
+    | S n', O => false
+    | S n', S m' => beq_nat n' m'
+  end.
+
+Fixpoint ble_nat (n m : nat) : bool :=
+  match n, m with
+    | O, _ => true
+    | S n', O => false
+    | S n', S m' => ble_nat n' m'
+  end.
+
+Example test_ble_nat1: (ble_nat 2 2) = true.
+Proof. reflexivity. Qed.
+Example test_ble_nat2: (ble_nat 2 4) = true.
+Proof. reflexivity. Qed.
+Example test_ble_nat3: (ble_nat 4 2) = false.
+Proof. reflexivity. Qed.
+
+(* Exercise: 2 stars (blt_nat) *)
+
+Definition blt_nat (n m : nat) : bool := (andb (ble_nat n m) (negb (beq_nat n m))).
+
+Example test_blt_nat1: (blt_nat 2 2) = false.
+Proof. reflexivity. Qed.
+Example test_blt_nat2: (blt_nat 2 4) = true.
+Proof. reflexivity. Qed.
+Example test_blt_nat3: (blt_nat 4 2) = false.
+Proof. reflexivity. Qed.
